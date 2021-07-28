f611ae3bd0d7ba325f9b914de113af097de211a8
KG01419586 <70454638+KG01419586@users.noreply.github.com>
2021-07-28 13:48:08 -0400

    l,

---
+++ Test/Mono_Test/Mono_Test.java
@@ -0,0 +1,17 @@
+package Mono_Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Mono.Board;
+
+class Mono_Test {
+
+	@Test
+	void testBoard() {
+		Board board = new Board();
+		while(true);
+	}
+
+}
